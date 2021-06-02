3049693ff509b9fd1148c0cc1a1c1c49198e8bb1
bossemel <18574762+bossemel@users.noreply.github.com>
2021-06-02 14:34:58 +0200

    fixed type bug

--- cond_indep_test.py
+++ cond_indep_test.py
@@ -1,6 +1,6 @@
 import numpy as np
 from marg_flow import Marginal_Flow
-from cop_flow import Cop_Flow
+from cop_flow import Cop_Flow 
 from exp_runner import Experiment
 from sklearn.model_selection import train_test_split
 from utils import set_optimizer_scheduler
@@ -9,14 +9,16 @@ from torch.utils.data import DataLoader
 import torch
 
 
-def marginal_transform_1D(inputs: np.ndarray, lr=0.001, weight_decay=0.00001,
-                       amsgrad=False, num_epochs=100, batch_size=128) -> np.ndarray:
+def marginal_transform_1d(inputs: np.ndarray, lr=0.001, weight_decay=0.00001,
+                          amsgrad=False, num_epochs=100, batch_size=128, num_workers=4) -> np.ndarray:
     # Transform into data object
     data_train, data_val = train_test_split(inputs, test_size=0.20)
     data_train = DataProvider(data_train)
-    loader_train = DataLoader(data_train, batch_size=64, shuffle=True, num_workers=4) # @Todo: take these arguments from somewhere else
+    loader_train = DataLoader(data_train, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+                                                        # @Todo: take these arguments from somewhere else
     data_val = DataProvider(data_val)
-    loader_val = DataLoader(data_val, batch_size=64, shuffle=True, num_workers=4) # @Todo: take these arguments from somewhere else
+    loader_val = DataLoader(data_val, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+                                                        # @Todo: take these arguments from somewhere else
 
     # Initialize marginal transform
     marg_flow = Marginal_Flow(n_layers=5)
@@ -45,10 +47,10 @@ def marginal_transform(inputs: np.ndarray, lr=0.001, weight_decay=0.00001,
     if inputs.shape[1] > 1:
         outputs = torch.empty_like(torch.from_numpy(inputs))
         for dim in range(inputs.shape[1]):
-            outputs[:, dim: dim + 1] = marginal_transform_1D(inputs[:, dim: dim+1], lr, weight_decay,
+            outputs[:, dim: dim + 1] = marginal_transform_1d(inputs[:, dim: dim+1], lr, weight_decay,
                        amsgrad, num_epochs, batch_size).reshape(-1, 1).detach()
     elif inputs.shape[1] == 1:
-        outputs = marginal_transform_1D(inputs, lr, weight_decay,
+        outputs = marginal_transform_1d(inputs, lr, weight_decay,
                        amsgrad, num_epochs, batch_size).reshape(-1, 1).detach()
     else:
         raise ValueError('Invalid input shape.')
@@ -57,15 +59,17 @@ def marginal_transform(inputs: np.ndarray, lr=0.001, weight_decay=0.00001,
 
 def copula_estimator(x_inputs: np.ndarray, y_inputs: np.ndarray,
                      cond_set: np.ndarray, lr=0.001, weight_decay=0.00001,
-                       amsgrad=False, num_epochs=100, batch_size=128) -> np.ndarray:
+                       amsgrad=False, num_epochs=100, batch_size=128):
     # Transform into data object
     inputs_cond = np.concatenate([x_inputs, y_inputs, cond_set], axis=1)
     data_train, data_val = train_test_split(inputs_cond, test_size=0.20)
     data_train = DataProvider(inputs=data_train[:, :2], cond_inputs=data_train[:, 2:])
-    loader_train = DataLoader(data_train, batch_size=64, shuffle=True, num_workers=4) # @Todo: take these arguments from somewhere else
+    loader_train = DataLoader(data_train, batch_size=64, shuffle=True, num_workers=4)
+                                            # @Todo: take these arguments from somewhere else
 
     data_val = DataProvider(inputs=data_val[:, :2], cond_inputs=data_val[:, 2:])
-    loader_val = DataLoader(data_val, batch_size=64, shuffle=True, num_workers=4) # @Todo: take these arguments from somewhere else
+    loader_val = DataLoader(data_val, batch_size=64, shuffle=True, num_workers=4)
+                                            # @Todo: take these arguments from somewhere else
 
     # Initialize Copula Transform
     cop_flow = Cop_Flow(n_layers=5, context_dim=cond_set.shape[1])
@@ -88,32 +92,37 @@ def copula_estimator(x_inputs: np.ndarray, y_inputs: np.ndarray,
 
     # Transform to uniform
     normal_distr = torch.distributions.normal.Normal(0, 1)
-    outputs = normal_distr.cdf(outputs)
+    outputs = normal_distr.cdf(outputs)  # @Todo: are these outputs needed?
 
-    return outputs
+    return outputs, experiment.model  # @Todo: recheck whether this model is then trained...
 
 
-def mi_estimator(cond_copula: np.ndarray) -> float:
+def mi_estimator(cop_flow) -> float:
     # @Todo: possibly also add ranges
     # @Todo: think about format of mutual information
+
+    # @Todo: sample from uniform for w
+    # @Todo: sample from cop flow given w
+    # @Todo: calculate conditional mutual information
+
     raise NotImplementedError
     # return mutual_information
 
 
-def hypothesis_test(mutual_information: float) -> bool:
+def hypothesis_test(mutual_information: float, threshold: float) -> bool: # @Todo: something like num obs?
     raise NotImplementedError
     # return result
 
 
 def copula_indep_test(x_input: np.ndarray, y_input: np.ndarray,
-                      cond_set: np.ndarray, num_epochs=100) -> bool: # @Todo: add flag for already transformed input, maybe even make every input a tuple
+                      cond_set: np.ndarray, num_epochs=100) -> bool:
+    # @Todo: add flag for already transformed input, maybe even make every input a tuple
 
     x_uni = marginal_transform(x_input, num_epochs=num_epochs)
     y_uni = marginal_transform(y_input, num_epochs=num_epochs)
     cond_uni = marginal_transform(cond_set, num_epochs=num_epochs)
 
-    cond_copula = copula_estimator(x_uni, y_uni, cond_uni, num_epochs=num_epochs)
-    raise NotImplementedError
+    cond_copula, cop_flow = copula_estimator(x_uni, y_uni, cond_uni, num_epochs=num_epochs)
 
     mutual_information = mi_estimator(cond_copula)
 
--- tests/test_cond_indep_test.py
+++ tests/test_cond_indep_test.py
@@ -1,5 +1,4 @@
 from cond_indep_test import copula_estimator
-import numpy as np
 import torch
 
 
@@ -9,9 +8,12 @@ def test_copula_estimator():
     y_uni = torch.FloatTensor(obs, 1).uniform_(0, 1)
     cond_uni = torch.FloatTensor(obs, 5).uniform_(0, 1)
 
-    copula = copula_estimator(x_uni, y_uni, cond_uni, num_epochs=1)
+    copula, __ = copula_estimator(x_uni, y_uni, cond_uni, num_epochs=1)
     assert torch.min(copula) >= 0
     assert torch.max(copula) <= 1
 
+# @Todo: write test for marginal estimator
+# @Todo: write test with uniform distr for copula estimator
+
 if __name__ == '__main__':
-    test_cond_indep_test()
+    test_copula_estimator()
